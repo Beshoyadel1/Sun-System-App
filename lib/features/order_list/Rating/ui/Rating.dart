4b4262d5ff06d16701445d88dbd13bdcbc694dd2
Beshoy Ael <beshoy.atwan@gmail.com>
2025-09-18 18:27:11 +0300

    Initial commit

--- lib/features/order_list/Rating/ui/Rating.dart
+++ lib/features/order_list/Rating/ui/Rating.dart
@@ -20,11 +20,15 @@ class RatingDialogWidget extends StatefulWidget {
 
 class _RatingDialogWidgetState extends State<RatingDialogWidget> {
   final TextEditingController _commentController = TextEditingController();
+  late RatingCubit cubit;
 
   @override
+  void initState() {
+    super.initState();
+    cubit = RatingCubit();
+  }
+  @override
   Widget build(BuildContext context) {
-    final cubit = RatingCubit();
-
     return Align(
       alignment: AlignmentGeometry.topCenter,
       child: SizedBox(
@@ -40,9 +44,59 @@ class _RatingDialogWidgetState extends State<RatingDialogWidget> {
                 borderRadius: BorderRadius.circular(12),
               ),
               padding: const EdgeInsets.all(16),
-              child: MultiBlocBuilder(
-                cubit: cubit,
-                commentController: _commentController,
+              child: Column(
+                spacing: 20,
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  const SizedBox(height: 10),
+                  TextInAppWidget(
+                    text: AppLanguageKeys.rateService,
+                    textSize: 14,
+                    fontWeightIndex: FontSelectionData.regularFontFamily,
+                    textColor: AppColors.darkColor,
+                  ),
+
+                  BlocBuilder<RatingCubit, RatingState>(
+                    bloc: cubit,
+                    buildWhen: (prev, curr) => prev.rating != curr.rating,
+                    builder: (context, state) {
+                      return RatingBar.builder(
+                        initialRating: state.rating,
+                        minRating: 1,
+                        allowHalfRating: false,
+                        itemCount: 5,
+                        itemSize: 40,
+                        unratedColor: AppColors.greyColor.withOpacity(0.5),
+                        itemBuilder: (_, __) =>
+                        const Icon(Icons.star, color: AppColors.orangeColor),
+                        onRatingUpdate: cubit.updateRating,
+                      );
+                    },
+                  ),
+
+                  FirstNameTextfieldPersonalDataWidget(
+                    name: AppLanguageKeys.writeComment,
+                    maxLines: 5,
+                    isRegular: true,
+                    textFormController: _commentController,
+                  ),
+
+                  const SizedBox(height: 10),
+                  ButtonWidget(
+                    text: AppLanguageKeys.addYourComment,
+                    textColor: AppColors.whiteColor,
+                    buttonColor: AppColors.orangeColor,
+                    textSize: 15,
+                    fontWeightIndex: FontSelectionData.regularFontFamily,
+                    heightContainer: 40,
+                    widthContainer: 300,
+                    borderRadius: 30,
+                    onTap: () {
+                      Navigator.pop(context);
+                    },
+                  ),
+                  const SizedBox(height: 10),
+                ],
               ),
             ),
           ),
@@ -52,72 +106,3 @@ class _RatingDialogWidgetState extends State<RatingDialogWidget> {
   }
 }
 
-
-class MultiBlocBuilder extends StatelessWidget {
-  final RatingCubit cubit;
-  final TextEditingController commentController;
-  const MultiBlocBuilder({
-    super.key,
-    required this.cubit,
-    required this.commentController,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      spacing: 20,
-      mainAxisSize: MainAxisSize.min,
-      children: [
-        const SizedBox(height: 10),
-        TextInAppWidget(
-          text: AppLanguageKeys.rateService,
-          textSize: 14,
-          fontWeightIndex: FontSelectionData.regularFontFamily,
-          textColor: AppColors.darkColor,
-        ),
-
-        BlocBuilder<RatingCubit, RatingState>(
-          bloc: cubit,
-          buildWhen: (prev, curr) => prev.rating != curr.rating,
-          builder: (context, state) {
-            return RatingBar.builder(
-              initialRating: state.rating,
-              minRating: 1,
-              allowHalfRating: false,
-              itemCount: 5,
-              itemSize: 40,
-              unratedColor: AppColors.greyColor.withOpacity(0.5),
-              itemBuilder: (_, __) =>
-              const Icon(Icons.star, color: AppColors.orangeColor),
-              onRatingUpdate: cubit.updateRating,
-            );
-          },
-        ),
-
-        FirstNameTextfieldPersonalDataWidget(
-          name: AppLanguageKeys.writeComment,
-          maxLines: 5,
-          isRegular: true,
-          textFormController: commentController,
-        //  onChanged: cubit.updateComment,
-        ),
-
-        const SizedBox(height: 10),
-        ButtonWidget(
-          text: AppLanguageKeys.addYourComment,
-          textColor: AppColors.whiteColor,
-          buttonColor: AppColors.orangeColor,
-          textSize: 15,
-          fontWeightIndex: FontSelectionData.regularFontFamily,
-          heightContainer: 40,
-          widthContainer: 300,
-          borderRadius: 30,
-          onTap: () {
-            Navigator.pop(context);
-          },
-        ),
-        const SizedBox(height: 10),
-      ],
-    );
-  }
-}
